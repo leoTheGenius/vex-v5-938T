bd71ddc2ccf05b0362fc2680fa23012806476a0c
Leo Wu <leowu1425@gmail.com>
2026-01-27 14:53:56 -0800

    made auton

--- vex938tcode/src/main.cpp
+++ vex938tcode/src/main.cpp
@@ -11,7 +11,8 @@ pros::MotorGroup leftdrive({3, 2, 1}, pros::v5::MotorGears::blue);
 pros::MotorGroup rightdrive({-9, -8, -10}, pros::v5::MotorGears::blue);
 pros::Motor intake(5, pros::v5::MotorGears::blue);
 pros::Motor outtake(-7, pros::v5::MotorGears::blue);
-
+pros::adi::DigitalOut load('H');
+pros::adi::DigitalOut wing('A');
 // pros::Rotation pidright(4);
 // pros::Rotation pidleft(6);
 enum Mode { no, a, b, r1, r2, down, right, x, y, l1, l2, up, left};
@@ -55,6 +56,7 @@ struct pidstuff {
 		return rightpos - leftpos;
 	}
 };
+
 double drivewheelgap = 12.5;
 int autonchoice = 0;
 
@@ -90,7 +92,7 @@ void competition_initialize() {
     while (pros::competition::is_disabled()) {
         if (master.get_digital_new_press(DIGITAL_A)) {
             autonchoice = 1;
-            master.print(2, 0, "auton left   ");
+            master.print(1, 0, "auton left   ");
         }
         if (master.get_digital_new_press(DIGITAL_B)) {
             autonchoice = 2;
@@ -98,7 +100,7 @@ void competition_initialize() {
         }
         if (master.get_digital_new_press(DIGITAL_Y)) {
             autonchoice = 3;
-            master.print(2, 0, "auton skills ");
+            master.print(3, 0, "auton skills ");
         }
         pros::delay(20);
     }
@@ -117,19 +119,19 @@ void competition_initialize() {
  */
  //should i remove speed?
  //actually no, for small spaces speed slow  might be goooooooooooooooooo0o00o0o00o00o0o000o000o0000o0o00oo00o0o0o0o0o0o00o0o0o0o0o0o0o0o0d
-void driveforward(double dist, double speed, const double posrange = 0.5, const double speedmulti = 1.0, const double kppos = 0.171, const double kipos = 0.037, const double kdpos = 0.1) {//!inches
+void driveforward(double dist, double speed, const double posrange = 0.3, const double timeout = 4000, const double speedmulti = 1.0, const double kppos = 0.271, const double kipos = 0.037, const double kdpos = 0.1) {//!inches
 	leftdrive.tare_position();
 	rightdrive.tare_position();
 	
 	pidstuff pid;
 	pid.reset();
 
-	const double kpdrift = 0.3;
-	const double kidrift = 0.03;
+	const double kpdrift = 0.35;
+	const double kidrift = 0.2;
 	const double kddrift = 0.03;
  
 	// const double posrange = 0.2; //!in tolerance for endpoint
-	const int cyclereq = 15;
+	const int cyclereq = 1;
 
 
 	const double toldeg = posrange * tickpin; 
@@ -150,19 +152,27 @@ void driveforward(double dist, double speed, const double posrange = 0.5, const 
 		pid.update();
 		std::int32_t currtime = pros::millis();
 		double dt = (currtime - prevtime) / 1000.0;
-		dt *= speedmulti;
 		if (dt <= 0.0) dt = 0.01;
 		prevtime = currtime;
 		double pos = pid.avgpos(); // wheel degrees
 		double poserror = targetdeg - pos; // degrees error
-		if (poserror < 0) {
-			if (posintegral > 0) {
-				posintegral = 0.0;
+		if (targetdeg >= 0) {
+			if (poserror < 0) {
+				if (posintegral > 0) {
+					posintegral = 0.0;
+				}
+			}
+		} else  {
+			if (poserror > 0) {
+				if (posintegral < 0) {
+					posintegral = 0.0;
+				}
 			}
 		}
 		master.print(0, 0, "error: %f", poserror);
 		posintegral += poserror * dt;
 		double posderiv = (poserror - pospreverr) / dt;
+		posderiv=posderiv/speedmulti;
 		pospreverr = poserror;
 		double posoutput = kppos * poserror + kipos * posintegral + kdpos * posderiv;
 		double speedlimit = std::clamp(std::abs(speed), 1.0, 127.0);
@@ -183,9 +193,9 @@ void driveforward(double dist, double speed, const double posrange = 0.5, const 
 		leftvoltage = std::clamp(leftvoltage, -127, 127);
 		rightvoltage = std::clamp(rightvoltage, -127, 127);
 
-		leftdrive.move(leftvoltage*0.8);
-		rightdrive.move(rightvoltage*0.8);
-		if (currtime - begintime > 4000) {
+		leftdrive.move(leftvoltage*speedmulti);
+		rightdrive.move(rightvoltage*speedmulti);
+		if (currtime - begintime > timeout) {
 			master.print(4, 0, "timeout");
 			break;
 		}	
@@ -221,7 +231,7 @@ void nopidback(double dist, double speed = 100.0) {
 	leftdrive.tare_position();
 	rightdrive.tare_position();
 	
-	const double tickpin = ticksperspin / (M_PI * wheeldiameter);
+	const double tickpin = -1*ticksperspin / (M_PI * wheeldiameter);
 	double targetdeg = dist * tickpin;
 	
 	while (leftdrive.get_position() > targetdeg || rightdrive.get_position() > targetdeg) {
@@ -231,7 +241,7 @@ void nopidback(double dist, double speed = 100.0) {
 	leftdrive.move(0);
 	rightdrive.move(0);
 }
-void oneside(double dist, bool leftside, double speed = 50.0) {
+void oneside(double dist, bool leftside, bool reverse, double speed = 50.0) {
 	leftdrive.tare_position();
 	rightdrive.tare_position();
 	
@@ -239,20 +249,35 @@ void oneside(double dist, bool leftside, double speed = 50.0) {
 	double targetdeg = dist * tickpin;
 	
 	if (leftside) {
-		while (leftdrive.get_position() < targetdeg) {
-			leftdrive.move(127*speed/100.0);
-			rightdrive.move(0);
+		if (reverse) {
+			while (leftdrive.get_position() > targetdeg) {
+				leftdrive.move(-127*speed/100.0);
+				rightdrive.move(0);
+			}
+		} else {
+			while (leftdrive.get_position() < targetdeg) {
+				leftdrive.move(127*speed/100.0);
+				rightdrive.move(0);
+			}
 		}
 	} else {
-		while (rightdrive.get_position() < targetdeg) {
-			leftdrive.move(0);
-			rightdrive.move(127*speed/100.0);
+		if (reverse) {
+			while (rightdrive.get_position() > targetdeg) {
+				leftdrive.move(0);
+				rightdrive.move(-127*speed/100.0);
+			}
+		} else {
+			while (rightdrive.get_position() < targetdeg) {
+				leftdrive.move(0);
+				rightdrive.move(127*speed/100.0);
+			}
 		}
+		
 	}
 	leftdrive.move(0);
 	rightdrive.move(0);
 }
-void turn(double degrees, double speed, double angletol = 0.7){
+void turn(double degrees, double speed, double angletol = 0.4){
 	// reset drivetrain motor encoder positions before starting the turn
 	leftdrive.tare_position();
 	rightdrive.tare_position();
@@ -264,18 +289,18 @@ void turn(double degrees, double speed, double angletol = 0.7){
 	pid.reset();
 
 	const double kpturn = 2.1;
-	const double kiturn = 0.2;
-	const double kdturn = 0.1;
+	const double kiturn = 0.0;
+	const double kdturn = 0.04;//if this doesnt work go back to 0.01
 
 	// const double angletol = 0.7; //! degrees range to be considerd finished wit hturn
-	const int stablereq = 15;
+	const int stablereq = 5;
 
 	double integral = 0.0;
 	double preverror = 0.0;
 	int stable = 0;
 	std::int32_t prevt = pros::millis();
 	std::int32_t startt = prevt;
-	const std::int32_t timeout_ms = 8000; // safety timeout to avoid infinite spin
+	const std::int32_t timeout_ms = 2500; // safety timeout to avoid infinite spin
 
 	while (true) {
 		pid.updateturn();
@@ -284,13 +309,20 @@ void turn(double degrees, double speed, double angletol = 0.7){
 		double dt = (now - prevt) / 1000.0;
 		if (dt <= 0.0) dt = 0.01;
 		prevt = now;
-
+		if (degrees > 0) {
+			if (integral > 0) {
+				integral = 0.0;
+			}
+		} else {
+			if (integral < 0) {
+				integral = 0.0;
+			}
+		}
 		double heading = pid.turndegrees(drivewheelgap);
 		double err = degrees - heading;
 
 		// Diagnostic output to controller for debugging
 		master.print(0, 0, "heading: %f", heading);
-
 		// safety timeout
 		if ((now - startt) > timeout_ms) {
 			master.print(4, 0, "Turn timeout (ms=%d)", timeout_ms);
@@ -337,33 +369,102 @@ void autonomous() {
 	outtake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 	if (autonchoice == 0) {
 		//!default auton/no auton selected
-		nopidback(10, 60);
-		nopid(20, 100);
+		// driveforward(29, 97, 0.9);
+		// turn(90, 87, 1.7);
+		// load.set_value(true);
+		// intake.move(600);
+		// nopid(10, 70);
+		// intake.move(600);
+		// driveforward(-24, 70, 6, 1.0, 5, 0.05, 0.01);
+		// load.set_value(false);
+		// outtake.move(600);
+		// pros::delay(1000);
+		// nopid(2, 50);
+		// oneside(27, true);
+		// driveforward(35, 127, 1.8);
+		// outtake.move(-600);
+		// intake.move(-600);
+		// for (int i = 0; i < 12; i++) {
+		// 	turn(90, 97, 1.7);
+		// }
+		// driveforward(-600, 127, 5);
+		// load.set_value(true);
+		// intake.move(600);
+		// nopid(25, 60);
+		// pros::delay(1000);
+		// driveforward(-100, 127, 8);
+		// outtake.move(600);
+		// pros::delay(1000);
+		// nopid(10, 50);
+		// turn(-30, 97, 5);
+		// nopid(70, 80);
+
+		wing.set_value(true);
+		intake.move(600);
+		driveforward(24, 127, 5, 700, 1.2);
+		driveforward(10.8, 107, 2, 1000, 1.3);
+		turn(51, 127, 0.8);
+
+		driveforward(13, 97, 1, 1500, 1.4);
+		load.set_value(true);
+		driveforward(-2, 127, 0.5, 600);
+		oneside(-40, true, true, 80);
+		driveforward(-40, 127, 20, 1000, 20);
+		nopid(7, 60);
+		driveforward(-40, 127, 10, 800);
+		outtake.move(600);
+		intake.move(600);
+		pros::delay(1500);
+		load.set_value(true);
+		outtake.move(0);
+		driveforward(27, 107, 2, 2000, 1.3);
+		pros::delay(1000);
+		driveforward(-36, 127, 10);
+		outtake.move(600);
+		// turn();
 	}
 	if (autonchoice == 1) {
 		//!left auton
 		driveforward(29, 97, 0.9);
 		turn(-90, 97, 1.7);
+		load.set_value(true);
+		intake.move(600);
 		driveforward(4, 127, 1.5);
 		pros::delay(1000);
 		driveforward(-24, 70, 6, 1.0, 5, 0.05, 0.01);
-		pros::delay(1000);
+		outtake.move(600);
+		load.set_value(false);
+		pros::delay(2000);
 		nopid(2, 50);
-		oneside(25, false);
+		outtake.move(0);
+		oneside(25, false, false);
 		nopid(6, 60);
 		pros::delay(1000);
 		turn(175, 97, 9);
+		outtake.move(-75);
 		driveforward(-25, 120, 1.8);
+		nopid(5, 70);
+		nopidback(5, 70);
+		nopid(5, 70);
+		nopidback(5, 70);
+		nopid(5, 70);
+		nopidback(5, 70);
+		nopid(5, 70);
+		nopidback(5, 70);
 	} else if (autonchoice == 2) {
 		//!right auton
 		driveforward(29, 97, 0.9);
 		turn(90, 87, 1.7);
+		load.set_value(true);
+		intake.move(600);
+		nopid(10, 70);
 		intake.move(600);
 		driveforward(-24, 70, 6, 1.0, 5, 0.05, 0.01);
+		load.set_value(false);
 		outtake.move(600);
 		pros::delay(1000);
 		nopid(2, 50);
-		oneside(27, true);
+		oneside(27, true, false);
 		driveforward(35, 127, 1.8);
 		outtake.move(-600);
 		intake.move(-600);
@@ -399,6 +500,7 @@ void opcontrol() {
 	// nopid(2, 50);
 	// oneside(27, true);
 	// driveforward(35, 127, 1.8);
+
 	leftdrive.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	rightdrive.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	intake.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
@@ -409,7 +511,7 @@ void opcontrol() {
 		int forward = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 		int turn = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 		leftdrive.move(forward + turn);
-		rightdrive.move(forward - turn);
+		rightdrive.move((forward - turn));
 		curra = master.get_digital(DIGITAL_A);
 		currb = master.get_digital(DIGITAL_B);
 		curry = master.get_digital(DIGITAL_Y);
@@ -462,7 +564,32 @@ void opcontrol() {
 				intake.move_velocity(0);
 			}
 		}
-		
+		else if ((currr1 == true) && (prevr1 == false)) {
+			activeMode = (activeMode == r1) ? no : r1;
+			if (activeMode == r1) {
+				load.set_value(true);
+			} else {
+				load.set_value(false);
+			}
+		}
+		else if ((currl1 == true) && (prevl1 == false)) {
+			activeMode = (activeMode == l1) ? no : l1;
+			if (activeMode == l1) {
+				wing.set_value(true);
+			} else {
+				wing.set_value(false);
+			}
+		}
+		else if (currl2 == true && prevl2 == false) {
+			activeMode = (activeMode == l2) ? no : l2;
+			if (activeMode == l2) {
+				leftdrive.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+				rightdrive.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			} else {
+				leftdrive.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+				rightdrive.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+			}
+		}
         
 		// update previous button states
 		preva = curra;
